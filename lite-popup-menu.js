d85acc2b4e2126d4356f29ff75585be97473ac1d
AntonBlg <35571014+AntonBlg@users.noreply.github.com>
2018-11-20 14:58:42 +0900

    Update lite-popup-menu.js

--- lite-popup-menu.js
+++ lite-popup-menu.js
@@ -1,1 +1,30 @@
-/* js-script */
+;(
+	function() {
+		
+		var lpm = {		
+		
+			bindPopup: function(target) {
+				
+				if (typeof target === "string") {
+					var target_list = document.querySelectorAll(target);
+					target_list.forEach(function(item) {
+						lpm.setAction(item);
+					});
+				}
+				else {
+					throw new Error("bind_popup: element with current target is not found");
+				}
+				
+			},
+			
+			setAction: function(id) {
+				id.addEventListener("contextmenu", function(event) {
+					event.preventDefault();
+					console.log("context menu!");
+				});
+			}
+		}
+		
+		window.$pm = lpm;
+	}
+)();
